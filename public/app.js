84fbfcf22b43c0bb6fb60c6cfb284816c0e3acd5
Kurt Madel <kmadel@mac.com>
2015-06-12 23:16:23 -0400

    update app.js to call same host on different port and support CORS, added Dockerfile

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://mobile-deposit-api/account/deposit"
+        url: window.location.protocol + "//" + window.location.host + ":8080/account/deposit",
+        dataType: "json"
     }).then(function(data) {
        $('.accountNumber').append(data.accountNumber);
        $('.balance').append(data.balance);
