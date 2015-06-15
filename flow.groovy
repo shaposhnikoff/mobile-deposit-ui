1d87b1b7c86c8267b4ee6a0f9810610d019744f6
Kurt Madel <kmadel@mac.com>
2015-06-15 10:41:54 -0400

    updated flow and Dockerfile

--- flow.groovy
+++ flow.groovy
@@ -7,7 +7,7 @@ node('docker') {
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/cloudbees/mobile-deposit-api.git']]])
                 sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo clean package'
 
-                stage 'integration-test'
+                stage 'functional-test'
                 //sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo verify'
                 //step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
 
@@ -18,19 +18,22 @@ node('docker') {
                     echo "Releaed version ${buildVersion}"
                 }
                 matcher = null
-                archive "target/mobile-deposit-api.jar, target/Dockerfile"
+                archive "target/mobile-deposit-ui.jar, target/Dockerfile"
             }
     }
-        unarchive mapping: ['target/mobile-deposit-api.jar' : '.', 'target/Dockerfile' : '.']
+    docker.withServer('tcp://54.173.235.97:2375') {
+        unarchive mapping: ['target/mobile-deposit-api.jar': '.', 'target/Dockerfile': '.']
         stage 'build docker image'
-        def mobileDepositApiImage = docker.build "kmadel/mobile-deposit-api:${dockerBuildTag}"
+        def mobileDepositApiImage = docker.build "mobile-deposit-ui:${dockerBuildTag}"
 
         stage 'deploy to production'
-        sh "docker run -d --name mobile-deposit-api -p 8080:8080 mobile-deposit-api:${dockerBuildTag}"
-
-        stage 'publish docker image'
-        docker.withRegistry('https://registry.hub.docker.com/', 'docker-registry-kmadel-login') {
-            //mobileDepositApiImage.push()
-            mobileDepositApiImage.push "${dockerBuildTag}"
+        try{
+            sh "docker stop mobile-deposit-ui"
+            sh "docker rm mobile-deposit-ui"
+        } catch (Exception _) {
+            echo "no container to stop"
         }
+        sh "docker run -d --name mobile-deposit-ui -p 80:8080 mobile-deposit-ui:${dockerBuildTag}"
+    }
+
 }
