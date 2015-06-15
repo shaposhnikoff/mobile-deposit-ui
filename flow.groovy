8384838294bf37cd8e8c5ed65411a0a277c92b46
Kurt Madel <kmadel@mac.com>
2015-06-15 10:43:39 -0400

    update git url for flow

--- flow.groovy
+++ flow.groovy
@@ -4,7 +4,7 @@ stage 'build'
 node('docker') {
     docker.withServer('tcp://127.0.0.1:1234'){
             docker.image('kmadel/maven:3.3.3-jdk-8').inside('-v /data:/data') {
-                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/cloudbees/mobile-deposit-api.git']]])
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/cloudbees/mobile-deposit-ui.git']]])
                 sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo clean package'
 
                 stage 'functional-test'
