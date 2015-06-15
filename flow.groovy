48ad289945a56a9e0eea03fe17beaeddb4a46e09
Kurt Madel <kmadel@mac.com>
2015-06-15 13:04:37 -0400

    updated flow, added input step and stage deployment

--- flow.groovy
+++ flow.groovy
@@ -8,10 +8,10 @@ node('docker') {
                 sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo clean package'
 
                 stage 'functional-test'
-                //sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo verify'
-                //step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
+                sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo verify'
+                step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
 
-                stage 'release'
+                stage 'prepare release'
                 def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
                 if (matcher) {
                     buildVersion = matcher[0][1]
@@ -25,6 +25,15 @@ node('docker') {
         unarchive mapping: ['target/mobile-deposit-ui.jar': '.', 'target/Dockerfile': '.']
         stage 'build docker image'
         def mobileDepositApiImage = docker.build "mobile-deposit-ui:${dockerBuildTag}"
+        try{
+            sh "docker stop mobile-deposit-ui-stage"
+            sh "docker rm mobile-deposit-ui-stage"
+        } catch (Exception _) {
+            echo "no container to stop"
+        }
+        sh "docker run -d --name mobile-deposit-ui-stage -p 82:8080 mobile-deposit-ui:${dockerBuildTag}"
+
+        input 'UI Staged at http://54.173.235.97:82/deposit - Proceed with Production Deployment?'
 
         stage 'deploy to production'
         try{
