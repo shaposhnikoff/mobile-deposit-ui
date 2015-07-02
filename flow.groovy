cb555a88b246e6d99f3ade4977cacdcb77d348db
Kurt Madel <kmadel@mac.com>
2015-07-02 10:44:41 -0400

    added docker-traceability/submitContainerStatus

--- flow.groovy
+++ flow.groovy
@@ -3,12 +3,12 @@ def buildVersion = null
 stage 'build'
 node('docker') {
     def mobileDepositUiImage
-    docker.withServer('tcp://127.0.0.1:1234'){
-            docker.image('kmadel/maven:3.3.3-jdk-8').inside('-v /data:/data') {
+    docker.withServer('tcp://127.0.0.1:1234'){ //run the following steps on this Docker host
+            docker.image('kmadel/maven:3.3.3-jdk-8').inside('-v /data:/data') { //use this image as the build environment
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/cloudbees/mobile-deposit-ui.git']]])
                 sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo clean package'
 
-                //get version
+                //get new version of application from pom
                 def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
                 if (matcher) {
                     buildVersion = matcher[0][1]
@@ -19,7 +19,7 @@ node('docker') {
     }
 
     //build image and deploy to staging
-    docker.withServer('tcp://54.165.201.3:2376', 'slave-docker-us-east-1-tls') {
+    docker.withServer('tcp://54.165.201.3:2376', 'slave-docker-us-east-1-tls') { //run following steps on our staging server
         stage 'build docker image'
         dir('target') {
             mobileDepositUiImage = docker.build "mobile-deposit-ui:${buildVersion}"
@@ -49,6 +49,7 @@ node('docker') {
             echo "no container to stop"
         }
         mobileDepositUiImage.run("--name mobile-deposit-ui -p 80:8080")
+        sh 'curl http://jenkins.beedemo.net/mobile-team/docker-traceability/submitContainerStatus --data-urlencode inspectData="$(docker inspect mobile-deposit-ui)"'
     }
 
 }
